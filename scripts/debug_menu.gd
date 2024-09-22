03dff176849533fb8fe6bd3bed69c0057b34ff6a
lucagoc <77584602+lucagoc@users.noreply.github.com>
2024-09-22 16:04:29 +0200

    [Debug] Ajout d'un menu de debug et d'un clavier virtuel

---
+++ scripts/debug_menu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Show all the system information
+	$SystemInfo.text = "OS: " + OS.get_name() + " " + OS.get_distribution_name() + " \nKernel: " + OS.get_version() + "\n" + "Processeur: " + str(OS.get_processor_count()) + " cores\n" + "RAM usage: " + str((OS.get_static_memory_usage()/1024)/1024) + " MB\n" + "Langue: " + OS.get_locale() 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass				
+
+
+func _on_volume_up_pressed() -> void:
+	# Exec command amixer set Master 10%+ 
+	OS.execute("/usr/bin/amixer", ["set", "Master", "10%+"])	
+
+
+func _on_volume_down_pressed() -> void:
+	# Exec command amixer set Master 10%- 
+	OS.execute("/usr/bin/amixer", ["set", "Master", "10%-"])
---
+++ scripts/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_option_button_pressed() -> void:
+	# Open debug_menu
+	get_tree().change_scene("res://scenes/debug_menu.tscn")
