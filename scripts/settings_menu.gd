dbd37dcc54beca604619d55859a9dfeed6e2a97d
lucagoc <77584602+lucagoc@users.noreply.github.com>
2024-10-01 23:42:21 +0200

    [Fix] Animation main settings

--- scripts/settings_menu.gd
+++ scripts/settings_menu.gd
@@ -1,10 +1,9 @@
 extends Control
 
+@onready var animation_player = $MainVbox/MainHbox/MainSettings/AnimationPlayer
 
 func _ready():
 	$MainVbox/MainHbox/MainSettings/System.grab_focus()
-
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
