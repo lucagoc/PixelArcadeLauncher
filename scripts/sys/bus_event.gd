6a46d2d85dc31a846dd7f4ae028688349883ce8e
lucagoc <77584602+lucagoc@users.noreply.github.com>
2024-10-23 00:21:32 +0200

    Update bus_event.gd

--- scripts/sys/bus_event.gd
+++ scripts/sys/bus_event.gd
@@ -5,66 +5,74 @@
 
 extends Node
 
+@warning_ignore("unused_signal")
 signal GAME_LIST_LOADED
-
+@warning_ignore("unused_signal")
 signal GAME_LAUNCHED(id: int)       # When a game is launched
+@warning_ignore("unused_signal")
 signal GAME_EXITED(id: int)         # When a game is exited
+@warning_ignore("unused_signal")
 signal GAME_SELECTED(id: int)       # When a game is selected (indepedent of the context menu)
+@warning_ignore("unused_signal")
 signal GAME_DESELECTED(id: int)     # When a game is deselected (indepedent of the context menu)
-
+@warning_ignore("unused_signal")
 signal DRAWER_RELOADED              # Drawer reloaded
+@warning_ignore("unused_signal")
 signal DRAWER_FOCUSED               # Drawer focused
+@warning_ignore("unused_signal")
 signal DRAWER_CATEGORY_OPENED       # Drawer category opened
+@warning_ignore("unused_signal")
 signal DRAWER_CATEGORY_CLOSED       # Drawer category closed
-
+@warning_ignore("unused_signal")
 signal BANNER_MENU_RELOADED         # Banner menu loaded/reloaded
+@warning_ignore("unused_signal")
 signal BANNER_MENU_FOCUSED          # Banner menu focused
 
 func _on_game_list_loaded() -> void:
-    print("[SIGNAL] Game list loaded")
+	print("[SIGNAL] Game list loaded")
 
 func _on_game_launched(id: int) -> void:
-    print("[SIGNAL] Game launched: ", id)
+	print("[SIGNAL] Game launched: ", id)
 
 func _on_game_exited(id: int) -> void:
-    print("[SIGNAL] Game exited: ", id)
+	print("[SIGNAL] Game exited: ", id)
 
 func _on_game_selected(id: int) -> void:
-    print("[SIGNAL] Game selected: ", id)
+	print("[SIGNAL] Game selected: ", id)
 
 func _on_game_deselected(id: int) -> void:
-    print("[SIGNAL] Game deselected: ", id)
+	print("[SIGNAL] Game deselected: ", id)
 
 func _on_drawer_reloaded() -> void:
-    print("[SIGNAL] Drawer reloaded")
+	print("[SIGNAL] Drawer reloaded")
 
 func _on_drawer_focused() -> void:
-    print("[SIGNAL] Drawer opened")
+	print("[SIGNAL] Drawer opened")
 
 func _on_drawer_category_opened() -> void:
-    print("[SIGNAL] Drawer category opened")
+	print("[SIGNAL] Drawer category opened")
 
 func _on_drawer_category_closed() -> void:
-    print("[SIGNAL] Drawer category closed")
+	print("[SIGNAL] Drawer category closed")
 
 func _on_banner_menu_reloaded() -> void:
-    print("[SIGNAL] Banner menu reloaded")
+	print("[SIGNAL] Banner menu reloaded")
 
 func _on_banner_menu_focused() -> void:
-    print("[SIGNAL] Banner menu focused")
+	print("[SIGNAL] Banner menu focused")
 
 func _ready() -> void:
-    connect("GAME_LIST_LOADED", _on_game_list_loaded)
+	connect("GAME_LIST_LOADED", _on_game_list_loaded)
 
-    connect("GAME_LAUNCHED", _on_game_launched)
-    connect("GAME_EXITED", _on_game_exited)
-    connect("GAME_SELECTED", _on_game_selected)
-    connect("GAME_DESELECTED", _on_game_deselected)
-    
-    connect("DRAWER_RELOADED", _on_drawer_reloaded)
-    connect("DRAWER_FOCUSED", _on_drawer_focused)
-    connect("DRAWER_CATEGORY_OPENED", _on_drawer_category_opened)
-    connect("DRAWER_CATEGORY_CLOSED", _on_drawer_category_closed)
+	connect("GAME_LAUNCHED", _on_game_launched)
+	connect("GAME_EXITED", _on_game_exited)
+	connect("GAME_SELECTED", _on_game_selected)
+	connect("GAME_DESELECTED", _on_game_deselected)
+	
+	connect("DRAWER_RELOADED", _on_drawer_reloaded)
+	connect("DRAWER_FOCUSED", _on_drawer_focused)
+	connect("DRAWER_CATEGORY_OPENED", _on_drawer_category_opened)
+	connect("DRAWER_CATEGORY_CLOSED", _on_drawer_category_closed)
 
-    connect("BANNER_MENU_RELOADED", _on_banner_menu_reloaded)
-    connect("BANNER_MENU_FOCUSED", _on_banner_menu_focused)
+	connect("BANNER_MENU_RELOADED", _on_banner_menu_reloaded)
+	connect("BANNER_MENU_FOCUSED", _on_banner_menu_focused)
