2449c07411a2c3f34c0ecf1f8ee44cae100b0a54
lucagoc <77584602+lucagoc@users.noreply.github.com>
2024-11-29 18:33:47 +0100

    [Engine] New proc execution for non-mame

--- scenes/common/process_manager.gd
+++ scenes/common/process_manager.gd
@@ -7,7 +7,11 @@ var game_running = false
 
 func set_game(id: int) -> void:
 	game_id = id
-	
+
+func launch_game_linux(game) -> void:
+	running_process = OS.execute(game.exec, [])
+
+
 func launch_game() -> void:
 	if game_id == -1:
 		print("[ERROR] No game selected")
@@ -19,9 +23,23 @@ func launch_game() -> void:
 			args = ["-homepath", Settings.general.mame_plugins_home]
 			print("[INFO] MAME homepath: " + Settings.general.mame_plugins_home)
 
-		# Launch the game
-		print("[INFO] Launching game: " + game.name)
-		running_process = OS.execute(game.exec, args)
+			# Launch the game
+			print("[INFO] Launching game: " + game.name)
+			running_process = OS.execute(game.exec, args)
+			# Emit the signal when exited
+			BusEvent.emit_signal("GAME_EXITED", game_id)
+		
+		elif game.platform == "linux":
+			# Create a new thread to launch the game
+			game_running = true
+			running_process = OS.create_process(game.exec, [])
+			print("[INFO] Game running with PID: " + str(running_process))
 
-		# Emit the signal when exited
-		BusEvent.emit_signal("GAME_EXITED", game_id)
+func _process(delta):
+	if Input.is_action_just_pressed("exit_game"):
+		print("[INFO] Exit game")
+		if game_running:
+			# Kill the game process
+			OS.kill(running_process)
+			game_running = false
+			BusEvent.emit_signal("GAME_EXITED", game_id)
